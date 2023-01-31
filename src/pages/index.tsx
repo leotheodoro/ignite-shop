8de21382f8117747a8a151d50c933f90156c28cd
Leonardo Theodoro <leonardo@recruitrobin.com>
2023-01-31 10:10:10 -0300

    💄 config stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import { styled } from "@/styles"
+
+const Button = styled('button', {
+  backgroundColor: "DarkBlue"
+})
+
 export default function Home() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Button>oi</Button>
   )
 }
