e19b63bb7c0bccbd580bce0f5058f1fbee3ee865
Leonardo Theodoro <leonardo@recruitrobin.com>
2023-01-31 10:31:51 -0300

    :lipstick: header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
+import { Container, Header } from '@/styles/pages/app'
 import { globalStyles } from '@/styles/styles'
 import type { AppProps } from 'next/app'
 
 globalStyles()
 
+import logoImg from '../assets/logo.svg'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+
+ 
+  )
 }
