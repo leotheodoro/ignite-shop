9733d34a08dbf4e728767322f03e07afd4cd1759
Leonardo Theodoro <leonardo@recruitrobin.com>
2023-02-01 09:49:47 -0300

    :construction: Home Structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,29 @@
-import { styled } from "@/styles"
+import Image from "next/image"
+import { HomeContainer, Product } from "@/styles/pages/home"
 
-const Button = styled('button', {
-  backgroundColor: "DarkBlue"
-})
+import shirt1 from '../assets/shirts/1.png'
+import shirt2 from '../assets/shirts/2.png'
+import shirt3 from '../assets/shirts/3.png'
 
 export default function Home() {
   return (
-    <h1>oi</h1>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={shirt2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta 2</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
