628ec62c6a59f96ccbd83ae0fc7ed3c33242547d
Leonardo Theodoro <leonardo@recruitrobin.com>
2023-02-07 21:25:17 -0300

    :construction: succcess page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,20 @@
+import { SuccessContainer, ImageContainer } from '@/styles/pages/success'
+import Link from 'next/link'
+
 export default function Success() {
-  return <h1>Sucesso</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul! <strong>Leonardo Theodoro</strong>, sua{' '}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho de sua
+        casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
 }
